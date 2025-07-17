99bec6afbd3d75613188008d5b4598d4c9f4a35d
Rishika-993 <rishikaagrawal22@gmail.com>
2025-07-18 00:59:34 +0530

    feat: add react-router-dom dependency and enhance LandingPage with welcome message and buttons

--- frontend/src/screens/LandingPage/LandingPage.js
+++ frontend/src/screens/LandingPage/LandingPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Row } from 'react-bootstrap'
+import { Button, Container, Row } from 'react-bootstrap'
 import './LandingPage.css';
 
 export const LandingPage = () => {
@@ -7,7 +7,20 @@ export const LandingPage = () => {
       <div className='main'>
           <Container>
               <Row>
-                  Landing
+                  <div className='intro-text'>
+                      <div className='text-white'>
+                          <h1 className='title'>Welcome to Note Maker</h1>
+                          <p className='subtitle'>One Safe place for all your notes.</p>
+                      </div>
+                      <div className='buttonContainer'>
+                          <a href="/login">
+                              <Button size='lg' className='landingButton'>Login</Button>
+                          </a>
+                          <a href="/register">
+                              <Button size='lg' className='landingButton' variant='outline-primary'>Register</Button>
+                          </a>
+                      </div>
+                  </div>
               </Row>
           </Container>
     </div>
