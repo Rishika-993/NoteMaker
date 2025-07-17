caf569ec3dd4f68524b2dbd254ad32f386c4023a
Rishika-993 <rishikaagrawal22@gmail.com>
2025-07-17 16:10:45 +0530

    Add package.json for Notemaker project with initial dependencies and scripts, created basic api to allocate notes

---
+++ backend/data/notes.js
@@ -0,0 +1,32 @@
+const notes = [
+    {
+      _id: "1",
+      title: "Experimented with Neural Networks",
+    content:
+      "Built a basic image classifier using TensorFlow. Understood forward propagation and backpropagation better.",
+    category: "Artificial Intelligence",
+    },
+    {
+      _id: "2",
+      title: "Deep Work Practice",
+    content:
+      "Tried Cal Newport's concept of 'Deep Work' by blocking 3 hours for focused coding. Productivity and mental clarity increased.",
+    category: "Productivity",
+    },
+    {
+      _id: "3",
+      title: "Cognitive Biases Study",
+      content:
+        "Learned about confirmation bias and survivorship bias. Helps in making better decisions by questioning my assumptions.",
+      category: "Psychology",
+    },
+    {
+      _id: 4,
+      title: "The Power of Compound Interest",
+      content:
+        "Studied how small, consistent investments grow massively over time. Applied this to habits too—small actions compound.",
+      category: "Finance",
+    },
+  ];
+  
+  module.exports = notes;
---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const notes = require('./data/notes');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+    const note = notes.find((n) => n._id === req.params.id);
+    res.send(note);
+});
+
+app.listen(5000, console.log('Server is running on port 5000'));
