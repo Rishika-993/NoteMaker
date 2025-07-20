521581de66b849d358b97121a8c6b1b95d55bf24
Rishika-993 <rishikaagrawal22@gmail.com>
2025-07-21 01:52:30 +0530

    feat: add login and registration screens with routing, api call and error handeling

---
+++ frontend/src/screens/LoginScreen/LoginScreen.js
@@ -0,0 +1,72 @@
+import MainScreen from '../../components/MainScreen'
+import { Button, Col, Form, Row } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import './LoginScreen.css'
+import { useState } from 'react'
+import axios from 'axios'
+
+const LoginScreen = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        try {
+            const config = {         //whenever api takes json request, we need to set the headers
+                headers: {
+                    'Content-type': 'application/json',
+                }
+            };
+            setLoading(true);
+
+            const { data } = await axios.post('/api/users/login', { email, password }, config);  //destructuring the data
+            console.log(data);
+            setLoading(false);
+            localStorage.setItem('userInfo', JSON.stringify(data)); //store user info in local
+        } catch (error) {
+            setError(error.response && error.response.data.message ? error.response.data.message : error.message);
+        }
+    }
+  return (
+    <MainScreen title="LOG IN">
+      <div className="loginContainer">
+        {/* {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+        {loading && <Loading />} */}
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="formBasicEmail" className="mb-3">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              value={email}
+              placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword" className="mb-3">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={password}
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+        <Row className="py-3">
+          <Col>
+            New Customer ? <Link to="/register">Register Here</Link>
+          </Col>
+        </Row>
+      </div>
+    </MainScreen>
+  )
+}
+
+export default LoginScreen
