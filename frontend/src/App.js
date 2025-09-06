418ceea4861faa5d2869bcdce2fd2c85c9918100
Rishika-993 <rishikaagrawal22@gmail.com>
2025-09-07 00:22:04 +0530

    style: enhance layout and styling across multiple components for improved responsiveness and visual consistency

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,7 +28,7 @@ const App = () => {
       </Routes>
     </main>
 
-    <Footer />
+    {/* <Footer /> */}
   </BrowserRouter>
 )}
 
--- frontend/src/components/MainScreen.js
+++ frontend/src/components/MainScreen.js
@@ -1,22 +1,26 @@
-import { Container, Row } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import './MainScreen.css'
 
 const MainScreen = ({ title, children }) => {
   return (
-      <div className='mainback'>
-          <Container>
-              <Row>
-                  <div className='page'>    
-                      {title && (
-                        <>
-                          <h1 className='heading'>{title}</h1>
-                          <hr />
-                        </>
-                      )}
-                      {children}
-                  </div>
-              </Row>
-          </Container>
+    <div className='mainback'>
+      <Container>
+        <Row className="justify-content-center">
+          <Col lg={10} xl={8}>
+            <div className='page'>    
+              {title && (
+                <>
+                  <h1 className='heading'>{title}</h1>
+                  <hr />
+                </>
+              )}
+              <div className="content-wrapper">
+                {children}
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
