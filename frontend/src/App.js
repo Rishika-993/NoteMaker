5d29f6e718915441f60d3850d0f1fb25fd8e1959
Rishika-993 <rishikaagrawal22@gmail.com>
2026-07-09 19:17:08 +0530

    Remove import statement for Footer component in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+// import Footer from './components/Footer/Footer';
 import { LandingPage } from './screens/LandingPage/LandingPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MyNotes from './screens/MyNotes/MyNotes';
